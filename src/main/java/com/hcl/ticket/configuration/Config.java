123e645b47d98bd6e4f8e0a35508534e39b75fbf
saurabh <saurabhguptaltr@gmail.com>
2020-12-30 15:56:13 +0530

    ticket

---
+++ src/main/java/com/hcl/ticket/configuration/Config.java
@@ -0,0 +1,30 @@
+package com.hcl.ticket.configuration;
+
+import javax.jms.Queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
+
+@Configuration
+public class Config {
+	@Value("${activemqurl}")
+	private String activeurl;
+
+	
+
+	@Bean
+	public ActiveMQConnectionFactory activeMQConnectionFactory() {
+		ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
+		activeMQConnectionFactory.setBrokerURL(activeurl);
+		return activeMQConnectionFactory;
+	}
+
+	@Bean
+	public JmsTemplate jmstemplate() {
+		return new JmsTemplate(activeMQConnectionFactory());
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO 
+	TBL_TICKET (user_id, train_id,status,number_of_ticket,ticket_booking_date,travelling_date ) 
+VALUES
+  	('userid', '12345', 'pending','3','2020-02-02','2020-02-02');
